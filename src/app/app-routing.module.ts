d5c50b9543d0a08661300db0b38acb50c902bffd
pplum09 <isara.ritthaworn@gmail.com>
2020-01-31 00:25:27 -0500

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
-  { path: 'home', component: HomeComponent },
-  { path: '**', redirectTo: 'home'},
+  { path: '', component: HomeComponent },
 ];
 
 @NgModule({
